1c0d9c8da684a6eb9e398f9b1c9c1ad462663834
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-03 18:47:28 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES('Mr. Chocolate',6,'M','blue','wild',true)
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES('Rowdy',8,'F','green','wild',true)
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES('Tabitha',45,'M','red','wild',false)
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES('Sergeant Brown',6,'F','green','wild',false)
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES('Melissa',7,'F','yellow','wild',true)
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES('Grinch',8,'F','red','wild',true)
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES('Wendy',4,'M','red','wild',false)
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES('Mr. Chocolate',6,'M','blue','wild',true),
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES('Rowdy',8,'F','green','wild',true),
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES('Tabitha',45,'M','red','wild',false),
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES('Sergeant Brown',6,'F','green','wild',false),
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES('Melissa',7,'F','yellow','wild',true),
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES('Grinch',8,'F','red','wild',true),
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES('Wendy',4,'M','red','wild',false),
 INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES(NULL,12,'F','green','wild',false)
